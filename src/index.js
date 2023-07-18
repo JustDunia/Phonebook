8783028b206892b60ee6464e833c6887cad8bde5
Justyna <justyna.sienkiewicz93@gmail.com>
2023-07-18 22:06:19 +0200

    base name

--- src/index.js
+++ src/index.js
@@ -8,12 +8,10 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={store} basename={'goit-react-hw-08-phonebook'}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
-
-// basename={'goit-react-hw-08-phonebook'}
